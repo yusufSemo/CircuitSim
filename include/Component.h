4869f0bafcdc1d317b9ea58141d4b18ea10fd51c
Yusuf Semo <yas3176@mavs.uta.edu>
2025-05-16 21:22:43 -0500

    Add base Component class and test in main

---
+++ include/Component.h
@@ -0,0 +1,41 @@
+//Component.h,
+//parent class that holds all the compnents we will use, like resistor, voltage Source,etc.
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include <vector>
+#include <initializer_list>
+class Circuit;  // Forward declaration
+class Component{
+    protected:
+        std::vector<int> terminals; //conneted nodes
+        int id; // for debugging
+    public:
+    // Supports: Component(1, {1, 2})
+    Component(int id_, std::initializer_list<int> nodeList)
+        : id(id_), terminals(nodeList) {}
+
+    // Supports: Component(1, std::vector<int>{1, 2})
+    Component(int id_, const std::vector<int>& nodeVec)
+        : id(id_), terminals(nodeVec) {}
+
+    virtual ~Component() = default;
+
+    // Access terminals
+    int getTerminal(size_t i) const { 
+        return terminals[i];
+    }
+    size_t numTerminals() const {
+        return terminals.size();
+    }
+    int getID() const {
+        return id;
+    }
+
+    // Every component must implement this to modify the matrix
+    virtual void stamp(Circuit& circuit) = 0;
+};
+
+
+#endif
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 //code by Yusuf Semo, 
 //working on circuit simulation software
+//test compile g++ -std=c++17 main.cpp -o sim
+
 
 #include <iostream>
-int main()
-{
-    std::cout << "hello circuit sim! " << std::endl;
+#include "../include/Component.h"
+
+// Dummy subclass just to test the base class
+class TestComponent : public Component {
+public:
+    TestComponent(int id, std::initializer_list<int> nodes)
+        : Component(id, nodes) {}
+
+    void stamp(Circuit& circuit) override {
+        // no-op
+    }
+};
+
+int main() {
+    TestComponent c1(1, {3, 5, 7});
+    std::cout << "Component ID: " << c1.getID() << "\n";
+    std::cout << "Terminals: ";
+    for (size_t i = 0; i < c1.numTerminals(); ++i) {
+        std::cout << c1.getTerminal(i) << " ";
+    }
+    std::cout << "\n";
     return 0;
 }
