ff8b772d9843c38e40c29653460ae4b841f416e0
Yusuf Semo <yas3176@mavs.uta.edu>
2025-05-19 11:06:47 -0500

    Add Resistor class and update main

---
+++ include/Resistor.h
@@ -0,0 +1,19 @@
+//RESISTOR.H
+#ifndef RESISTOR_H
+#define RESISTOR_H
+#include "Component.h"
+class Resistor : public Component {
+    private:
+        double resistance;
+    public:
+        Resistor(int id_, std::initializer_list<int> nodes, double r)
+            :Component(id_, nodes), resistance(r){std::cout << "resistor created"<<std::endl;}
+        double getR(){
+            return resistance;
+        }
+        void stamp(Circuit& circuit){}
+    
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-//code by Yusuf Semo, 
 //working on circuit simulation software
 //test compile g++ -std=c++17 main.cpp -o sim
 
 
 #include <iostream>
 #include "../include/Component.h"
+#include "../include/Resistor.h"
 
 // Dummy subclass just to test the base class
 class TestComponent : public Component {
@@ -17,6 +17,7 @@ public:
     }
 };
 
+
 int main() {
     TestComponent c1(1, {3, 5, 7});
     std::cout << "Component ID: " << c1.getID() << "\n";
@@ -25,5 +26,13 @@ int main() {
         std::cout << c1.getTerminal(i) << " ";
     }
     std::cout << "\n";
+    Resistor r1(3,{1,2}, 150);
+    std::cout << "the resistance is "<<r1.getR() << std::endl;
+    std::cout << "the id is "<<r1.getID() << std::endl;
+    std::cout << "terminal 0 is" << r1.getTerminal(0) << std::endl;
+    std::cout << "terminal 1 is " << r1.getTerminal(1) << std::endl;
+
+    
+
     return 0;
 }
